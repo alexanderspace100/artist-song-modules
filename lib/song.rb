0fc4a69d8baea2276ba5dd1d2d1294f566e37bbb
alexanderspace100 <alexanderspace100@gmail.com>
2016-07-27 19:08:37 +0300

    done

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song
+  attr_accessor :name
+  attr_reader :artist
+  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  extend Findable
+
+  @@songs = []
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def self.all
+    @@songs
+  end
+end
